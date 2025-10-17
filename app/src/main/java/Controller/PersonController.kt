5b19b1c8877a03a63384cecaa37d3734d94041fb
EngelAlexey <alexhmanzanares@gmail.com>
2025-10-16 20:21:24 -0600

    Commit

--- app/src/main/java/Controller/PersonController.kt
+++ app/src/main/java/Controller/PersonController.kt
@@ -1,4 +1,26 @@
 package Controller
 
+import Data.IDataManager
+import Data.MemoryDataManager
+import Entity.Person
+import android.content.Context
+
 class PersonController {
+
+    private var dataManager: IDataManager = MemoryDataManager
+    private var context: Context
+
+    constructor(context: Context){
+        this.context=context
+    }
+
+    fun addPerson(person: Person){
+        try {
+            dataManager.add(person)
+        } catch (e: Exception){
+            throw e
+        }
+
+    }
+
 }
