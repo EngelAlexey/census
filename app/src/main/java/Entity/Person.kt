76650f6ea97687360ffbeed1c49f468db736ed38
EngelAlexey <alexhmanzanares@gmail.com>
2025-10-16 18:31:58 -0600

    Commit

--- app/src/main/java/Entity/Person.kt
+++ app/src/main/java/Entity/Person.kt
@@ -8,7 +8,9 @@ class Person {
     private var id:                 String  =""
     private var name:               String  =""
     private var fLastName:          String  =""
+    private var sLastName:          String  =""
     private var phone:              Int     =0
+    private var email:              String  =""
     private lateinit var brithday:  Date
     private lateinit var province:  Province
     private var state:              String  =""
@@ -18,5 +20,29 @@ class Person {
     private var longitude:          Int     =0
     private lateinit var photo:     Bitmap
 
-    constructor(name: String)
+    constructor(
+        name: String,
+        flastname: String,
+        slastname: String,
+        phone: Int,
+        email: String,
+        brithday: Date,
+        province: Province,
+        state: String,
+        district: String
+    )
+
+    {
+        this.name       =   name
+        this.fLastName  =   flastname
+        this.sLastName  =   slastname
+        this.phone      =   phone
+        this.email      =   email
+        this.brithday   =   brithday
+        this.province   =   province
+        this.state      =   state
+        this.district   =   district
+    }
+
+
 }
