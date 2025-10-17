1c94288d4c6ca24de4734e6a857a920d01fb463b
EngelAlexey <alexhmanzanares@gmail.com>
2025-10-16 18:22:43 -0600

    Create: Class person and Province.kt

---
+++ app/src/main/java/Entity/Person.kt
@@ -0,0 +1,20 @@
+package Entity
+
+import android.graphics.Bitmap
+import java.sql.Date
+
+class Person {
+
+    private var id:                 String  =""
+    private var name:               String  =""
+    private var fLastName:          String  =""
+    private var phone:              Int     =0
+    private lateinit var brithday:  Date
+    private lateinit var province:  Province
+    private var state:              String  =""
+    private var district:           String  =""
+    private var address:            String  =""
+    private var latitude:           Int     =0
+    private var longitude:          Int     =0
+    private lateinit var photo:     Bitmap
+}
